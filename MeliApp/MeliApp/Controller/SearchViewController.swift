53842b9d8954fa9ef91199fee965ed2d7774193c
Leandro Higa <lean_higa@hotmail.com>
2020-12-21 13:54:42 -0300

    SearchViewController UI

---
+++ MeliApp/MeliApp/Controller/SearchViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  MeliApp
+//
+//  Created by Lean Caro on 21/12/2020.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self
+        
+        //Use light mode
+        if #available(iOS 13.0, *){
+            overrideUserInterfaceStyle = .light
+        }
+        
+        //Move keyboard up when open
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        //Dismiss keyboard when tap anywhere on the screen
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+   
+        //Add done button to keyboard toolbar
+        setupTextFields()
+    }
+    
+    func setupTextFields() {
+        let toolbar = UIToolbar()
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneButtonTapped))
+        
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+        toolbar.sizeToFit()
+        
+        searchTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneButtonTapped() {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+}
+
+
+
+//MARK: - UITextFieldDelegate
+
+extension SearchViewController: UITextFieldDelegate {
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        
+        //Dismiss keyboard when search button is pressed
+        searchTextField.endEditing(true)
+        
+        if let safeSearchTextField = searchTextField.text {
+            print(safeSearchTextField)
+        }
+    }
+    
+    //Execute when the keyboard's return button is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        //Dismiss keyboard
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+}
